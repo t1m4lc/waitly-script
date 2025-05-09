92992c3aef360e1d38f502c57776bf8772bce288
t1m4lc <t1m4lc@users.noreply.github.com>
2025-05-09 23:56:52 +0000

    deploy: ad6567c3a3f9fa347c93ad15658c11fd0ae0ef14

--- waitly-snippet.umd.cjs
+++ waitly-snippet.umd.cjs
@@ -1,1 +1,1 @@
-(function(e){typeof define=="function"&&define.amd?define(e):e()})(function(){"use strict";function e(){const n=document.getElementById("waitly");n?n.textContent="Hello from snippet!":console.warn("Element with id 'waitly' not found.")}e()});
+(function(n){typeof define=="function"&&define.amd?define(n):n()})(function(){"use strict";(function(){function n(){const e=document.getElementById("waitly");e?e.textContent="Hello from snippet!":console.warn("Element with id 'waitly' not found.")}n()})()});
